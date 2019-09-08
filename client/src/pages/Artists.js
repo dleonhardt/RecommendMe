0cce7d8f8b7447088e61aa30c6df8e95aca626e3
dleonhardt <dleonhardt01@gmail.com>
2019-09-08 18:48:22 -0400

    Began implementing Spotify API

--- client/src/pages/Artists.js
+++ client/src/pages/Artists.js
@@ -60,7 +60,8 @@ class Artists extends Component {
                 <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr className="my-4" />
                 <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <a className="btn btn-success btn-lg" href="/api/spotify" role="button">Connect to Spotify <i className="fa fa-spotify"></i>
+                </a>
               </div>
             </Jumbotron>
           </Col>
